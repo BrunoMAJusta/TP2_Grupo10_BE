342d9207bc3073d087ae405946c106ac86d27d5a
RuiFCS <9150564@esmad.ipp.pt>
2020-06-04 11:20:25 +0100

    aads

--- routes/epiRouter.js
+++ routes/epiRouter.js
@@ -1,9 +1,12 @@
 const Router = require("express").Router;
 var router = Router();
-const middleware = require("../middleware.js");
+const middleware = require("../middleware.js"); 
 
 const epiController = require("../controllers/epi/epiControllers");
 
 router.post('/epis', epiController.addEpi)
 
 router.get('/epis', epiController.getEpis)
+router.get('/epis/:id',middleware.checkToken, epiController.getEpi)
+
+module.exports = router
